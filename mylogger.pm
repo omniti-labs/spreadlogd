85bb44eeb01bb8ffd8a16b9187e1f707c3df7be9
jesus <jesus@8faaf92d-b214-0410-9274-84e98549e447>
2003-02-27 16:09:17 +0000

    comment out DBI, leave in for example

--- mylogger.pm
+++ mylogger.pm
@@ -2,7 +2,7 @@
 
 package mylogger;
 
-use DBI;
+#use DBI;
 use POSIX;
 use vars qw/$dbh $sth/;
 
@@ -10,17 +10,18 @@ sub log($$$) {
     my $sender = shift;
     my $group = shift;
     my $message = shift;
+    chomp($message);
 
-    $dbh ||= DBI->connect("DBI:mysql:database=weblogs", "logger", "", { RaiseError => 0 });
-    warn "DBI->connect failed." unless($dbh);
-    if($dbh) {
-        $sth ||= $dbh->prepare(q{INSERT INTO logs (host, domain, timestamp, data) VALUES(?,?,?,?)});
-    }
+#    $dbh ||= DBI->connect("DBI:mysql:database=weblogs", "logger", "", { RaiseError => 0 });
+#    warn "DBI->connect failed." unless($dbh);
+#    if($dbh) {
+#        $sth ||= $dbh->prepare(q{INSERT INTO logs (host, domain, timestamp, data) VALUES(?,?,?,?)});
+#    }
 
     my ($user, $host) = ($sender =~ /#([^#]+)#([^#]+)/);
     my ($time) = ($message =~ /\[([^\]]+)\]/);
     $time ||= stftime("%d/%b/%Y:%H:%M:%S %z", localtime(time));
-    $sth && $sth->execute($host, $group, $time, $message);
+#    $sth && $sth->execute($host, $group, $time, $message);
     print STDERR "Insert($host, $group, $time, $message);\n";
 }
 1;
